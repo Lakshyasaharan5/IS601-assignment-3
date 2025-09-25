2eaa204c41231f1de197b0f09e437759e1db3ab1
Lakshya Saharan <lakshyasaharan@gmail.com>
2025-09-24 21:50:02 -0400

    Implement subtraction function

--- calculator/operations.py
+++ calculator/operations.py
@@ -7,3 +7,9 @@ class Operations:
         """
         return a + b 
 
+    @staticmethod
+    def subtraction(a: float, b: float) -> float:
+        """
+        This static method takes two numbers (a and b) and returns their difference (a - b).        
+        """
+        return a - b 
