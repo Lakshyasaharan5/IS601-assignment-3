6ab624f83bc55910fbf92772f4585ae55f8f58ed
Lakshya Saharan <lakshyasaharan@gmail.com>
2025-09-24 21:53:18 -0400

    Implement division function with error handling

--- calculator/operations.py
+++ calculator/operations.py
@@ -20,3 +20,14 @@ class Operations:
         This static method takes two numbers (a and b) and returns their product (a * b).        
         """
         return a * b
+
+    @staticmethod
+    def division(a: float, b: float) -> float:
+        """
+        This static method takes two numbers (a and b) and returns their quotient (a / b).        
+        Raises error if divided by zero
+        """
+        if b == 0:            
+            raise ValueError("Can't divide by zero.")
+        return a / b  
+
