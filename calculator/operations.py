51060a29f36f6a41d2ef61d05184dc768fe9b334
Lakshya Saharan <lakshyasaharan@gmail.com>
2025-09-24 21:47:04 -0400

    add Operation class and implement addition function

--- calculator/operations.py
+++ calculator/operations.py
@@ -1,1 +1,9 @@
-This file will have calculator functions
+class Operations:
+
+    @staticmethod
+    def addition(a: float, b: float) -> float:
+        """
+        This static method takes two numbers (a and b) and returns their sum (a + b).
+        """
+        return a + b 
+
